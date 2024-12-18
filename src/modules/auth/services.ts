84cc00a0e3c4da79a3d2845e225c257050a3e76e
Rolando Diaz <rdiaz@promarisco.com>
2024-12-18 13:36:52 -0500

    Repositorio del proyecto enviado por Marlon

---
+++ src/modules/auth/services.ts
@@ -0,0 +1,24 @@
+// ? authServices
+// ? auth_services
+// ? AuthServices - PascalCase
+
+import AuthRepository from "./repository";
+
+export class AuthServices {
+    
+    private readonly _authRepository: AuthRepository
+    constructor(){
+        this._authRepository = new AuthRepository();
+    }
+
+    async registerService(username: string, password: string) {
+        const existingUser = await this._authRepository.findByUsername(username);
+        if(existingUser){
+            throw new Error('El usuario ya existe')
+        }
+
+        const newUser = await this._authRepository.createUser({username, password});
+        return newUser;
+
+    }
+}
